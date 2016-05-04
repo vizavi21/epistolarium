981f1ab215adf08da08080ee8d0544f80d15e2a4
vizavi21 <vizavi21@gmail.com>
2016-05-05 00:07:25 +0200

    Fourth commit

--- js/index.js
+++ js/index.js
@@ -68,24 +68,238 @@
   angular.module('angularDemo').service('ProductDataService', function() {
     var service = {};
 
-    //sample data
+    //data
     var products = [
       {
-        name: 'Chrysoloras to XYZ',
+        name: 'Manuel Chrysoloras to Coluccio Salutati',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        sender: 'Cyriaco',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1397-1398',
         properties: [
             { name:'sender', value:'Manuel Chrysoloras' },
-            { name:'addressee', value:'Guarino' },
+            { name:'addressee', value:'Coluccio Salutati' },
             { name:'type', value:'friendly' }, 
             { name:'language', value:'Greek' }, 
             { name:'size', value:'medium' }, 
-            { name:'genre', value:'long' }            
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Coluccio Salutati',
+        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1397-1399',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Coluccio Salutati' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      },{
+        name: 'Manuel Chrysoloras to Maximos Chrysoberges',
+        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1398-1399',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Maximos Chrysoberges' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      },{
+        name: 'Manuel Chrysoloras to Pope Innocent VII during the Western Schism',
+        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1404',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Pope Isidore VII' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Leonardo Bruni',
+        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1410',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Leonardo Bruni' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Manuel II Palaiologos',
+        content: 'Comparison between Rome and New Rome.',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1411',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Manuel II Palaiologos' },
+            { name:'type', value:'panegyrical' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to John Chrysoloras',
+        content: 'To his nephew',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1411',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'John Chrysoloras' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Demetrios Chrysoloras',
+        content: 'To his ...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1412',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Demetrios Chrysoloras' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Palla Strozzi',
+        content: 'To his ...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1413',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Palla Strozzi' },
+            { name:'type', value:'consolatory' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Guarino Veronese',
+        content: 'To his ...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1412',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Guarino Veronese' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Guarino Veronese',
+        content: 'To his two...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1412',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Guarino Veronese' },
+            { name:'type', value:'literary' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Uberto Decembrio',
+        content: 'To his two...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1413',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Uberto Decembrio' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Ambrogio Traversari',
+        content: 'To his two...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1413',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Ambrogio Traversari' },
+            { name:'type', value:'literary' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Palla Strozzi',
+        content: 'To his two...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1412',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Palla Strozzi' },
+            { name:'type', value:'friendly' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
+        ]
+      }, {
+        name: 'Manuel Chrysoloras to Manuel II Palaiologos',
+        content: 'To his two...',
+        sender: 'Manuel Chrysoloras',
+        connectedWith: 'Manuel II Palaiologos, Guarino Veronese, Demetrios Kydones',
+        context: 'XYZcontext',
+        date: '1414',
+        properties: [
+            { name:'sender', value:'Manuel Chrysoloras' },
+            { name:'addressee', value:'Manuel II Palaiologos' },
+            { name:'type', value:'panegyrical' }, 
+            { name:'language', value:'Greek' }, 
+            { name:'size', value:'medium' }, 
+            { name:'genre', value:'n/a' }            
         ]
       },{
         name: 'Filelfus to Guarino',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         sender: 'Cyriaco',
+        connectedWith: 'Valla, Manuel, XYZ',
         properties: [
             { name:'sender', value:'Franciscus Filelfus' },
             { name:'addressee', value:'Guarino' },
@@ -97,6 +311,7 @@
         name: 'Guarino to Chrysoloras',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         sender: 'Cyriaco',
+        connectedWith: 'Valla, Manuel, XYZ',
         properties: [
             { name:'sender', value:'Guarino Veronese' },
             { name:'addressee', value:'Manuel Chrysoloras' },
@@ -109,6 +324,7 @@
         name: 'Argyropoulos to Lorenzo de Medici',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         sender: 'Cyriaco',
+        connectedWith: 'Valla, Manuel, XYZ',
         properties: [
             { name:'sender', value:'John Argyropoulos' },
             { name:'addressee', value:'Lorenzo de Medici' },
@@ -121,6 +337,7 @@
         name: 'Isidore to Bessarion',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         sender: 'Cyriaco',
+        connectedWith: 'Valla, Manuel, XYZ',
         properties: [
             { name:'sender', value:'Isidore of Kiev' },
             { name:'addressee', value:'Bessarion' },
@@ -133,6 +350,7 @@
         name:'Colluccio to Kydones',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         sender: 'Cyriaco',
+        connectedWith: 'Valla, Manuel, XYZ',
         properties: [
             { name:'sender', value:'Colluccio Salutati' },
             { name:'addressee', value:'Demetrios Kydones' },
@@ -145,6 +363,7 @@
         name:'Cyriaco to Gattilusio',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         sender: 'Cyriaco',
+        connectedWith: 'Valla, Manuel, XYZ',
         properties: [
             { name:'sender', value:'Cyriaco of Ancona' },
             { name:'addressee', value:'Francesco Gattilusio' },
