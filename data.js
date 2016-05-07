5307fa7cfae7b30081a7d078e35ce72484cd1998
vizavi21 <vizavi21@gmail.com>
2016-05-07 15:09:36 +0200

    Eight commit

--- data.js
+++ data.js
@@ -1,74 +1,4 @@
-(function(){
-  'use strict'
-  angular.module('angularDemo', ['ngMaterial']); //initialize module
-
-  //can reference model instead of creating a global variable
-  angular.module('angularDemo').controller('angularController', 
-                                           ['$scope','ProductDataService', function($scope, ProductDataService) { 
-    var products = ProductDataService.getSampleData();
-    $scope.Fruits = products; // expose to the view
-
-    //create checkbox filters
-    var filters = [];
-    _.each(products, function(product) {
-      _.each(product.properties, function(property) {      
-        var existingFilter = _.findWhere(filters, { name: property.name });
-
-        if (existingFilter) {
-          var existingOption = _.findWhere(existingFilter.options, { value: property.value });
-          if (existingOption) {
-            existingOption.count += 1;
-          } else {
-            existingFilter.options.push({ value: property.value, count: 1 }); 
-          }   
-        } else {
-          var filter = {};
-          filter.name = property.name;
-          filter.options = [];
-          filter.options.push({ value: property.value, count: 1 });
-          filters.push(filter);      
-        }   
-      });
-    });
-    $scope.Filters = filters;
-
-    this.toggleAll = function($event, includeAll) {       
-      _.each(filters, function(filterCategory) {      
-        _.each(filterCategory.options, function(option) {
-          option.IsIncluded = includeAll;
-        });
-      });    
-    };
-  }]);
-
-  angular.module('angularDemo').filter('dynamicFilter', function () {
-    return function (products, filterCategories, scope) {
-      var filtered = [];
-
-      var productFilters = _.filter(filterCategories, function(fc) {
-        return  _.any(fc.options, { 'IsIncluded': true });
-      });
-
-      _.each(products, function(prod) {
-        var includeProduct = true;
-        _.each(productFilters, function(filter) {
-          var props = _.filter(prod.properties, { 'name': filter.name });
-          if (!_.any(props, function(prop) { return _.any(filter.options, { 'value': prop.value, 'IsIncluded': true }); })) {
-            includeProduct = false;
-          }
-        });
-        if (includeProduct) {
-          filtered.push(prod);
-        }
-      });
-      return filtered;
-    };
-  });
-
-  angular.module('angularDemo').service('ProductDataService', function() {
-    var service = {};
-
-    //data
+//data
     var products = [
       {
         name: 'Manuel Chrysoloras to Coluccio Salutati',
@@ -374,13 +304,3 @@
         ]
       }     
     ];
-
-
-    service.getSampleData = function() {
-      return products;
-    };
-
-    return service;
-  });
-
-})();
